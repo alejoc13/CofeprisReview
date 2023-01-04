bf64179b5234ed172c6e4af171e78e13275f96bc
rodrij545 <rodrij545@medtronic.com>
2023-01-04 09:24:56 -0500

    simplificación de la herramienta

--- helper/workflow.py
+++ helper/workflow.py
@@ -5,9 +5,7 @@ import pandas as pd
 def review():
     mx,sp,cof = pr.PrepareData()
     pr.searchdiff(cof,sp)
-    Coincidence,noCoincidence = pr.comparaDates(mx,cof)
-    cfn_coincidence = pr.recoverCFNs(Coincidence,mx)
-    pr.create_excelSearch(noCoincidence,Coincidence,cfn_coincidence,sp)
+  
     
 
 
